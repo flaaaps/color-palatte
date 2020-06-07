5d2eb5c12cd9b96ce7aa8c6a6f19afdc7895bc3a
flaaaps <58341821+flaaaps@users.noreply.github.com>
2020-06-07 12:08:50 +0200

    Attached missing documentations

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const str = "The best things in life are free";
 const patt = new RegExp("^free");
 const res = patt.exec(str);
-
-console.log(res)
-
 const input = document.getElementById('value-add')
+
 input.value = ""
 
+
+// Listening for Enter key
 input.addEventListener("keyup", function (e) {
     if (e.keyCode === 13) {
         const color = input.value
         const wrapper = document.getElementById('wrapper')
+
+        //Check if input is correctly formatted
         if (/^#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$/i.test(color)) {
-            console.log()
+            //Append box to wrapper
             wrapper.innerHTML += `
                          <div style='background: ${color}' class=\"color\">
                               <div class="inner">
@@ -23,11 +25,13 @@ input.addEventListener("keyup", function (e) {
                          </div>`
 
         } else if(color !== "") {
+            // Add not valid item
             wrapper.innerHTML += `<div style="color: white; display: flex; justify-content: center; align-items: center"
                               class=\"color not-valid\" id="not-valid">
                              <span style="text-align: center">Not a valid<br> hex code</span>
                              <input class=\"hidden-value\" type=\"text\" value=\"not valid\">
                          </div>`
+            // Remove it after 2 seconds
             setTimeout(function () {
                 let element = document.getElementById("not-valid");
                 element.parentNode.removeChild(element);
@@ -37,7 +41,9 @@ input.addEventListener("keyup", function (e) {
     }
 })
 
+// Remove a item from the grid
 function removeItem(el) {
+    el.parentNode.parentElement.style.transform = "translateY(50px)"
     el.parentNode.parentElement.style.opacity = "0"
     setTimeout(function () {
         el.parentNode.parentElement.remove()
