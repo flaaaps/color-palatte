92ecbe9c7ad97c6af281ef486eb906a4f6bdded0
flaaaps <58341821+flaaaps@users.noreply.github.com>
2020-06-07 04:13:53 +0200

    Design changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ input.addEventListener("keyup", function (e) {
     if (e.keyCode === 13) {
         const color = input.value
         const wrapper = document.getElementById('wrapper')
-        if (/^#[0-9A-F]{3,6}$/i.test(color)) {
+        if (/^#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$/i.test(color)) {
             console.log()
             wrapper.innerHTML += `
                          <div style='background: ${color}' class=\"color\">
@@ -22,10 +22,10 @@ input.addEventListener("keyup", function (e) {
                              </div>
                          </div>`
 
-        } else {
+        } else if(color !== "") {
             wrapper.innerHTML += `<div style="color: white; display: flex; justify-content: center; align-items: center"
                               class=\"color not-valid\" id="not-valid">
-                             <span style="text-align: center" oncanplay="removeItem(this)">Not a valid<br> hex code</span>
+                             <span style="text-align: center">Not a valid<br> hex code</span>
                              <input class=\"hidden-value\" type=\"text\" value=\"not valid\">
                          </div>`
             setTimeout(function () {
